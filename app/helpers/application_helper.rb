3d4d3a5635a4c32ba73d2e5c2e9c4d1a3d78b77f
Mike McCormick <mike.mccormick@pic-appalachia.com>
2012-07-19 15:53:38 -0400

    Fixed formatting in ApplicationHelper again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
 	# Returns the full title on a per-page basis
-	# formatting is fixed
 	def full_title(page_title)
 		base_title = "Ruby on Rails Tutorial Sample App"
 		if page_title.empty?
